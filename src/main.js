bdce82ec6b774a62beb031f730a1321ea9b9068b
shuaidi <heshuaidi123@gmail.com>
2022-10-04 20:27:29 +0800

    fold structure & main.js

--- src/main.js
+++ src/main.js
@@ -3,10 +3,20 @@ import 'core-js/stable'
 import 'regenerator-runtime/runtime'
 import es6promise from 'es6-promise'
 es6promise.polyfill()
+
+import iView from 'iview'
+import 'iview/dist/styles/iview.css'
+import locale from 'iview/src/locale/lang/zh-CN'
+
+import '@/styles/index.css'
+
 import App from './App.vue'
+
+Vue.use(iView, { locale })
 
 Vue.config.productionTip = false
 
 new Vue({
+  el: '#app',
   render: h => h(App),
 }).$mount('#app')
