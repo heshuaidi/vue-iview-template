8b528e0e29c1a3ea06f73ab9d223ec3f357cb084
shuaidi <heshuaidi123@gmail.com>
2022-10-04 10:47:33 +0800

    install es6-promise for IE browser

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import 'core-js/stable'
 import 'regenerator-runtime/runtime'
+import es6promise from 'es6-promise'
+es6promise.polyfill()
 import App from './App.vue'
 
 Vue.config.productionTip = false
